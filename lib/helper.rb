38af161619fb3feb54b9d0137e81d97d0a3676f4
Ryan Wood <ryan.wood@gmail.com>
2012-10-18 10:56:25 -0400

    add content for sourcescape.com

---
+++ lib/helper.rb
@@ -0,0 +1,33 @@
+module Helper
+  # call-seq:
+  #    breadcrumbs( page )    => html
+  #
+  # Create breadcrumb links for the current page. This will return an HTML
+  # <ul></ul> object.
+  #
+  def breadcrumbs( page )
+    list = ["<li>#{h(page.title)}</li>"]
+    loop do
+      page = @pages.parent_of(page)
+      break if page.nil?
+      list << "<li>#{link_to_page(page)}</li>"
+    end
+    list.reverse!
+
+    html = "<ul class=\"breadcrumbs\">\n"
+    html << list.join("\n")
+    html << "\n</ul>\n"
+    html
+  end
+  
+  def title(page)
+    s = "Sourcescape"
+    s += " | #{page.title}" if page.title && page.title != 'Home'
+    s
+  end
+    
+end  # module Helper
+
+Webby::Helpers.register(Helper)
+
+# EOF
